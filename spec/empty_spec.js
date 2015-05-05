e3e73cd487874afa0440687ae21cbda6f94d533d
Owen Smith <owen@omsmith.ca>
2015-05-05 10:43:23 -0400

    feat(default-handler) allow user to specify a default request handler

--- spec/empty_spec.js
+++ spec/empty_spec.js
@@ -1,15 +1,46 @@
-/* global describe, it */
+/* global afterEach, beforeEach, describe, it */
 
 'use strict';
 
-var router = new (require('../'))().listen(),
-	request = require('supertest')(router._server);
+var request = require('supertest');
+
+var Router = new require('../');
 
 describe('router', function () {
-	it('should 404 with no routes defined', function (done) {
-		request
+	var router;
+
+	beforeEach(function () {
+		router = new Router().listen();
+	});
+
+	afterEach(function () {
+		router._server.close();
+	});
+
+	it('should 404 with no routes defined, by default', function (done) {
+		request(router._server)
 			.get('/')
 			.expect(404)
+			.end(done);
+	});
+
+	it('should call specified default handler with no routes defined', function (done) {
+		var handlerCalled = false;
+		router.default(function (req, res) {
+			handlerCalled = true;
+
+			res.statusCode = 200;
+			res.end();
+		});
+
+		request(router._server)
+			.get('/')
+			.expect(200)
+			.expect(function () {
+				if (!handlerCalled) {
+					throw new Error('expected custom handler to be called');
+				}
+			})
 			.end(done);
 	});
 });
